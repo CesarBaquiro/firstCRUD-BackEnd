93b823a20daf9e23c04c828faa41f7bfe699823b
Cesar Manuel Baquiro Munoz <cesar.baquiro@globant.com>
2023-01-26 21:12:51 -0500

    Update config

--- demo/src/main/java/com/example/demo/controllers/UsuarioController.java
+++ demo/src/main/java/com/example/demo/controllers/UsuarioController.java
@@ -1,20 +1,23 @@
 package com.example.demo.controllers;
 
-
 import java.util.ArrayList;
+import java.util.logging.Logger;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import com.example.demo.DemoApplication;
 import com.example.demo.models.UsuarioModel;
 import com.example.demo.services.UsuarioService;
 
-
-
+import ch.qos.logback.classic.Level;
 
 @RestController
 @RequestMapping("/usuario")
 public class UsuarioController {
+	
+	//private final static Logger LOGGER = Logger.getLogger(UsuarioController.class);
+	
 	@Autowired
 	UsuarioService usuarioService;
 	
@@ -26,6 +29,7 @@ public class UsuarioController {
 	
 	@PostMapping()
 	public UsuarioModel guardarUsuario(@RequestBody UsuarioModel usuario) {
+		//LOGGER.log(Level.INFO, "Se hizo el post");
 		return this.usuarioService.guardarUsuario(usuario);
 	}
 }
